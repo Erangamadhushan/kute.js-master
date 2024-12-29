298ec8c41db27fd5d2a8a0387501988e3166ab4e
Erangamadhushan <erangamadhushann956@gmail.com>
2024-12-29 20:58:35 +0530

    Enhance user experience by updating sign-in form styles and adding background image, and improve discount section layout in product display

--- kute.js-master/src/pages/scripts/signinForm.js
+++ kute.js-master/src/pages/scripts/signinForm.js
@@ -7,21 +7,19 @@ function renderSignUpForm() {
         <div>
             <h1 class="text-center text-green-400 text-4xl font-extrabold">Geocery.com</h1>
         </div>
-        <div class="w-[700px] mx-auto mt-4 border border-[3px] rounded-xl border-green-400  grid justify-center items-center min-h-60 ">
+        <div class="w-[700px] mx-auto mt-4 border border-[3px] rounded-xl border-green-400  grid justify-center items-center min-h-60 userSigninPage">
             <div>
                 <h2 class="text-center text-green-400 text-3xl mt-4 font-bold">Sign In</h2>
             </div>
-            <form action="" autocomplete="on">
+            <form action="" autocomplete="on" class="min-h-90">
                 <div class="grid w-[390px] gap-5 my-10">
-                    <label for="username" class="w-full text-lg">Username:</label>
-                    <input type="text" class="w-full p-3 rounded text-lg border border-lg border-gray-400 hover:outline-none focus:border-green-400" id="username" name="username" required/>
+                    <input type="text" class="w-full p-3 rounded text-lg border border-lg border-gray-400 hover:outline-none focus:border-green-400" id="username" name="username" placeholder="User Name..." required/>
                 </div>
                 <div class="grid w-[390px] gap-5 my-10">
-                    <label for="userpassword" class="w-full text-lg">Password :</label>
-                    <input type="password" class="w-full rounded p-3 text-lg border border-lg border-gray-400 hover:outline-none focus:border-green-400" id="userpassword" name="userpassword" required/>
+                    <input type="password" class="w-full rounded p-3 text-lg border border-lg border-gray-400 hover:outline-none focus:border-green-400" id="userpassword" name="userpassword" placeholder="Password.." required/>
                 </div>
                 <div class="grid w-[390px] my-10">
-                    <input type="submit" value="Sign In" class="p-3 text-lg rounded-lg cursor-pointer text-white bg-green-400"/>
+                    <input type="submit" value="Sign In" class="p-3 text-lg rounded-lg cursor-pointer text-white bg-green-400 hover:bg-green-500"/>
                 </div>
                 
             </form>
