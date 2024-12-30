c64286b503a53500d3620124952162883f71c7f4
Erangamadhushan <erangamadhushann956@gmail.com>
2024-12-30 18:42:16 +0530

    Add navigation panel to multiple pages and implement slide toggle functionality

--- kute.js-master/src/pages/scripts/jqueryAction.js
+++ kute.js-master/src/pages/scripts/jqueryAction.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#navbar-icon").click(() => {
+    $("navbar-icon").click(() => {
         $("#panel").slideToggle();
     })
 })
--- kute.js-master/src/pages/scripts/navbar.js
+++ kute.js-master/src/pages/scripts/navbar.js
@@ -20,10 +20,10 @@ function renderNavBar() {
         </div>
     `;
     navBarContent += `
-        <div class="flex flex-row space-x-5 px-5 ">
+        <div class="flex flex-row space-x-5 px-5">
             <a href="#" class="hidden sm:block"><img src="./icons/navbar/add-to-cart.png" alt="add to card images"/></a>
-            <button type="button" onclick="signin()" class="p-2 px-4 border rounded-full text-green-400 border-green-400 hover:bg-green-400 hover:text-white hidden sm:block">Sign In</button>
-            <button type="button" class="p-2 px-4 border rounded-full text-green-400 border-green-400 hover:bg-green-400 hover:text-white block custom:hidden">&#9776;</button>
+            <button type="button" onclick="signin()" class="p-2 px-4 border rounded-full text-green-400 border-green-400 hover:bg-green-400 hover:text-white hidden md:block">Sign In</button>
+            <button type="button" class="p-2 px-4 border rounded-full text-green-400 border-green-400 hover:bg-green-400 hover:text-white block custom:hidden" id="navbar-icon">&#9776;</button>
         </div>
     `;
     
@@ -33,3 +33,15 @@ function renderNavBar() {
 function signin() {
     window.location.replace("./signin.html");
 }
+
+// Load nav bar into the main html files
+
+document.querySelector('#panel').innerHTML = `
+    <nav class="justify-center items-center grid grid-cols-1 block  px-3 custom:hidden">
+        <a href="./index.html" class="text-xl font-semibold hover:font-bold text-center py-3" >Home</a>
+        <a href="./aboutUs.html" class="text-xl font-semibold hover:font-bold text-center py-3">About Us</a>
+        <a href="./services.html" class="text-xl font-semibold hover:font-bold text-center py-3">Services</a>
+        <a href="./products.html" class="text-xl font-semibold hover:font-bold text-center py-3">Products</a>
+        <a href="./contact.html" class="text-xl font-semibold hover:font-bold text-center py-3">FAQ</a>
+    </nav>
+`;
