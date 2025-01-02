e0ff915d02ca4d8ad9143da42772e0a54d032845
Erangamadhushan <erangamadhushann956@gmail.com>
2025-01-02 12:30:09 +0530

    Add purchaseItem functionality and update button actions across product pages

--- kute.js-master/src/pages/scripts/action.js
+++ kute.js-master/src/pages/scripts/action.js
@@ -1,3 +1,21 @@
-function purchaseItem() {
-    console.log("Function called !");
+function purchaseItem(img, title, desc, price, discount) {
+    let item = {
+        img,
+        title,
+        desc,
+        price,
+        discount
+    }
+    let cart = localStorage.getItem('cart');
+    if(cart == null) {
+        cart = [];
+    }
+    else {
+        cart = JSON.parse(cart);
+    }
+    cart.push(item);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert("Item added to cart");
+
+    window.location.replace("buyItem.html");
 }
